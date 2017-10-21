727a9602d245719a92b5d93c8029603015b33fc1
tkn <tyson_ngo@hotmail.com>
2017-10-20 23:54:00 -0400

    fixed JSON cleaup script

--- cleanup.py
+++ cleanup.py
@@ -1,30 +1,29 @@
 from glob import glob
 from os import path, sep
-from wordlist import get_wordlist_json, update_wordlist_json
+from WordList import WordList
 import json
 import re
 
 
 class CleanUp(object):
 	def __init__(self, the_json):
-		self.alphabet_dict = get_wordlist_json(the_json)
+		self.words = WordList(the_json).words
 		self.initial_word_count = self.get_total_words()
 		self.filename = "cleanup"+sep+path.basename(the_json).replace(".json", "_cleanup.json")
-
-
+		self.indent = 4
+ 
 	def get_total_words(self):
 		words = 0
-		for c in self.alphabet_dict:
-			for word in self.alphabet_dict[c]:
+		for c in self.words:
+			for word in self.words[c]:
 				words += 1
 		return words
 
-
-	def remove_regex_from_alphabet_dict(self, regex, reverse_match=False, debug=False):
+	def remove_regex_from_words(self, regex, reverse_match=False, debug=False):
 		words = []
 		removed = 0
-		for c in self.alphabet_dict:
-			for word in self.alphabet_dict[c]:
+		for c in self.words:
+			for word in self.words[c]:
 				match = re.findall(regex, word) if reverse_match else not re.findall(regex, word)
 				if match:
 					words.append(word)
@@ -34,68 +33,59 @@ class CleanUp(object):
 					removed += 1
 			#if debug == True:
 			#	print(words)			
-			self.alphabet_dict[c][:] = words
+			self.words[c][:] = words
 			words[:] = []
 		return removed
 
-
 	def remove_prefixes(self):
-		return self.remove_regex_from_alphabet_dict("-$")
-
+		return self.remove_regex_from_words("-$")
 
 	def remove_suffixes(self):
-		return self.remove_regex_from_alphabet_dict("^-")
-
+		return self.remove_regex_from_words("^-")
 
 	def remove_non_roman_alphabets(self):
-		return self.remove_regex_from_alphabet_dict("^[a-zA-Z-'\.\s]+$", reverse_match=True)
-
+		return self.remove_regex_from_words("^[a-zA-Z-'\.\s]+$", reverse_match=True)
 
 	def remove_abbreviations(self):
-		return self.remove_regex_from_alphabet_dict("^[A-Z]+$")
-
+		return self.remove_regex_from_words("^[A-Z]+$")
 
 	def remove_multiple_words(self):
-		return self.remove_regex_from_alphabet_dict("\s")
-
+		return self.remove_regex_from_words("\s")
 
 	def remove_apostrophes(self):
-		return self.remove_regex_from_alphabet_dict("'")
-
+		return self.remove_regex_from_words("'")
 
 	def remove_periods(self):
-		return self.remove_regex_from_alphabet_dict("\.")
-
+		return self.remove_regex_from_words("\.")
 
 	def update(self):
-		JSON_NEW_FILENAME = self.filename
-		update_wordlist_json(self.alphabet_dict, JSON_NEW_FILENAME)
+		with open(self.filename, "w") as f:
+			json.dump(self.words, f, indent=self.indent)
 
 
+if __name__ == "__main__":
+	def print_removed_from(cleanup):
+		def print_removed(to_remove):
+			removed = getattr(cleanup, "remove_"+to_remove)()
+			print("Removed", removed, "words with", to_remove.replace("_"," "))
+		return print_removed
 
-def print_removed_from(cleanup):
-	def print_removed(to_remove):
-		removed = getattr(cleanup, "remove_"+to_remove)()
-		print("Removed", removed, "words with", to_remove.replace("_"," "))
-	return print_removed
+	filenames = glob("words/*.json")
 
+	for filename in filenames:
+		cleanup = CleanUp(filename)
+		remove = print_removed_from(cleanup)
+		print("Cleaning up", filename)
 
-filenames = glob("words/*.json")
-
-for filename in filenames:
-	cleanup = CleanUp(filename)
-	remove = print_removed_from(cleanup)
-	print("Cleaning up", filename)
-
-	remove("prefixes")
-	remove("suffixes")
-	remove("abbreviations")
-	remove("apostrophes")
-	remove("periods")
-	remove("non_roman_alphabets")
-	remove("multiple_words")
-	print("Remaining word count:\t", cleanup.get_total_words())
-	print("Total word count:\t", cleanup.initial_word_count)
-	
-	print ("Writing new JSON to", cleanup.filename)
-	cleanup.update()
+		remove("prefixes")
+		remove("suffixes")
+		remove("abbreviations")
+		remove("apostrophes")
+		remove("periods")
+		remove("non_roman_alphabets")
+		remove("multiple_words")
+		print("Remaining word count:\t", cleanup.get_total_words())
+		print("Total word count:\t", cleanup.initial_word_count)
+		
+		print ("Writing new JSON to", cleanup.filename)
+		cleanup.update()
