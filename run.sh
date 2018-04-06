752748fb0167f9f57056870f6423e846a0aba295
T <tyson_ngo@hotmail.com>
2018-04-06 14:23:41 -0400

    added run bash script

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+	echo
+	echo "Usage: ./run.sh <wikidump>"
+	echo
+	echo "    where <wikidump> is a path to an XML file"
+	echo "    of a Wikipedia dump"
+	echo
+	echo "Download zipped XML file from https://meta.wikimedia.org/wiki/Data_dump_torrents#English_Wikipedia"
+	exit 1
+fi
+
+if ! { [ -d "wikiextractor" ] && [ -f "wikiextractor/WikiExtractor.py" ]; }; then
+	echo
+	echo "Missing: wikiextractor/WikiExtractor.py"
+	git clone https://github.com/attardi/wikiextractor.git
+fi
+
+wikiextractor="wikiextractor/WikiExtractor.py"
+options="-b 500K -o extracted"
+
+python $wikiextractor $1 $options > wikiextractor.log 2>&1 &
+sleep 10
+node index.js > server.log 2>&1 &
+python main.py > words.log 2>&1 &
