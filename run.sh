98c10cc89a14c325cf11e02d1cbaac1207d51adf
T <tyson_ngo@hotmail.com>
2018-04-06 14:26:00 -0400

    update run script

--- run.sh
+++ run.sh
@@ -22,4 +22,7 @@ options="-b 500K -o extracted"
 python $wikiextractor $1 $options > wikiextractor.log 2>&1 &
 sleep 10
 node index.js > server.log 2>&1 &
+sleep 10
 python main.py > words.log 2>&1 &
+
+tail -f words.log
